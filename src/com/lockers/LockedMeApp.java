e5d2a67f0a451ab88e3aa261aaeddb83deadee4e
Nithya Subramanian <nithya.subramanian@thirdware.com>
2021-08-14 15:47:33 +0530

    Added Comment

--- src/com/lockers/LockedMeApp.java
+++ src/com/lockers/LockedMeApp.java
@@ -48,27 +48,45 @@ public class LockedMeApp {
 		}
 		
 	}
-	
+	/**
+	 * This method will retrieve all file names in the directory
+	 * @param path
+	 */
 	public static void getAllFiles(String path) {
+		//Retrieve all file names from the directory
 		List<String> fileNames=FileUtil.listAllFiles(FOLDER_PATH);
+		
+		//If list is not empty and null , print the file name
 		if(fileNames!=null && !fileNames.isEmpty())
 			fileNames.forEach(elt->System.out.println(elt));
 		else
 			System.out.println("Folder is empty");
 	}
 	
+	/**
+	 * This method will save the content to a file by creating a new file
+	 * @param obj
+	 * @param FOLDER_PATH
+	 */
 	public static void saveFile(Scanner obj,String FOLDER_PATH) {
+		//Read file name from the user
 		System.out.println("Enter the filename");
 		String fileName=obj.next();
+		
+		//Read the number of lines from the user
 		System.out.println("Enter the number of lines");
 		int line_count=obj.nextInt();
+		
+		//Read the lines from the user
 		List<String> lines=new ArrayList<String>();
 		for(int i=0;i<line_count;i++) {
-			System.out.println("Enter the line");
+			System.out.println("Enter the line "+i);
 			String line=obj.next();
 			lines.add(line);
 			
 		}
+		
+		//Return true if content got saved to file sucessfully
 		boolean isAdded=FileUtil.addFile(FOLDER_PATH, fileName, lines);
 		if(isAdded)
 			System.out.println("File and Data saved successfully");
@@ -76,16 +94,32 @@ public class LockedMeApp {
 			System.out.println("some error has occured.Please contact system administrator");
 		
 	}
+	/**
+	 * This method will delete the specified file
+	 * @param FOLDER_PATH
+	 * @param fileName
+	 */
 	public static void deleteFile(String FOLDER_PATH, String fileName) {
+		
+		//Delete the user specified file if exists and return true if success
 		boolean isDeleted=FileUtil.deleteFile(FOLDER_PATH, fileName);
+		
 		if(isDeleted)
 			System.out.println("File deleted successfully");
 		else
 			System.out.println("File may not exists");
 		
 	}
+	/**
+	 * This method will search for user specified file
+	 * @param FOLDER_PATH
+	 * @param fileName
+	 */
 	public static void searchFile(String FOLDER_PATH, String fileName) {
+		
+		//Return true if the user specified file exists else return false
 		boolean isFound=FileUtil.searchForFile(FOLDER_PATH, fileName);
+		
 		if(isFound)
 			System.out.println("File is found");
 		else
--- src/com/lockers/util/FileUtil.java
+++ src/com/lockers/util/FileUtil.java
@@ -15,18 +15,26 @@ public class FileUtil {
 	 * @return List<String>
 	 */
 	public static List<String> listAllFiles(String path) {
+		//creating file object
 		File folder = new File(path);
+		
+		//Declare the list to store file names
 		List<String> fileNamesList = null;
+		
 		// checking it is a folder or not
 		if (folder.isDirectory()) {
-			// Pull all files from the path
+			
+			// Getting all files into Array
 			File[] files = folder.listFiles();
+			
 			if (files.length > 0) {
 				fileNamesList = new ArrayList<String>();
+				
 				// Looping files and add file names to list
 				for (File file : files) {
 					fileNamesList.add(file.getName());
 				}
+				
 				// sort filenames
 				Collections.sort(fileNamesList);
 			}
@@ -46,10 +54,12 @@ public class FileUtil {
 	 * @return boolean
 	 */
 	public static boolean addFile(String path, String filename, List<String> content) {
+		//Creating Filewriter object
 		try(FileWriter writer= new FileWriter(path+File.separator+filename); )
 		{
+			//Looping content to write into file
 			for(String text:content) {
-				writer.write(text);
+				writer.write(text+"\n");
 			}
 				
 			return true;
@@ -59,9 +69,20 @@ public class FileUtil {
 		}
 	}
 	
+	/**
+	 * This method will delete the user specified if exists
+	 * 
+	 * @param path
+	 * @param filename
+	 * @return boolean
+	 */
 	public static boolean deleteFile(String path, String filename) {
 		boolean flag=false;
+		
+		//creating file object
 		File file=new File(path+File.separator+filename);
+		
+		//check whether file exists or not and also ensure case sensitivity search on file name
 		 if(fileExistsCaseSensitive(file)) {
 			 flag=file.delete();
 			 
@@ -69,14 +90,29 @@ public class FileUtil {
 		return flag;
 		
 	}
+	/**
+	 * This method will return true if user specified exists else it return false
+	 * @param path
+	 * @param filename
+	 * @return boolean
+	 */
 	public static boolean searchForFile(String path, String filename) {
+		//creating file object
 		File file=new File(path+File.separator+filename);
+		
+		//check whether file exists ,also ensure case sensitivity search on file name and return true if exists
 		return fileExistsCaseSensitive(file);
 		
 	}
 	
+	/**
+	 * This method will ensure case sensitivity on file name and return true if matches
+	 * @param file
+	 * @return boolean
+	 */
 	public static boolean fileExistsCaseSensitive(File file) {
 	    try {
+	    	//check whether file exists and also ensure case sensitivity search on file name and return true if exists
 	        return file.exists() && file.getCanonicalFile().getName().equals(file.getName());
 	    } catch (IOException e) {
 	        return false;
